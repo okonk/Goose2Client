7ce3f7580c0a93969577288e64b1968aa0abe9b2
Hayden Curran <haydenz@gmail.com>
2023-04-19 18:36:28 +1200

    Clean up chat bubble code. This also fixed an issue with the positioning of the text

--- Assets/Scripts/ChatBubble.cs
+++ Assets/Scripts/ChatBubble.cs
@@ -15,45 +15,37 @@ namespace Goose2Client
 
         public float Height => background.size.y;
 
+        private const float maxWidth = 250 / 32f;
+        private static readonly Vector2 padding = new Vector2(7f, 5f) / 32f;
+
         private void Start()
         {
             Invoke(nameof(DestroyBubble), 3);
-        }
-
-        internal void SetText(string message)
-        {
-            this.text.text = message;
-            this.text.ForceMeshUpdate();
-
-            var textSize = this.text.GetRenderedValues(false);
-            var padding = new Vector2(7f, 5f) / 32f;
-
-            float offsetX = 0.25f;
-
-            float maxWidth = 250 / 32f;
-
-            if (textSize.x >= maxWidth)
-            {
-                var rect = this.text.GetComponent<RectTransform>();
-                rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, maxWidth);
-
-                this.text.enableWordWrapping = true;
-                this.text.ForceMeshUpdate();
-                textSize = this.text.GetRenderedValues(false);
-            }
-            else
-            {
-                offsetX = -textSize.x / 2;
-            }
-
-            background.size = textSize + padding;
-            text.transform.localPosition = new Vector3(offsetX, textSize.y / 2);
         }
 
         private void DestroyBubble()
         {
             if (gameObject != null)
                 Destroy(gameObject);
+        }
+
+        internal void SetText(string message)
+        {
+            text.text = message;
+            text.ForceMeshUpdate();
+
+            var textSize = text.GetRenderedValues(false);
+            if (textSize.x > maxWidth)
+            {
+                text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, maxWidth);
+
+                text.enableWordWrapping = true;
+                text.ForceMeshUpdate();
+                textSize = text.GetRenderedValues(false);
+            }
+
+            text.rectTransform.sizeDelta = textSize;
+            background.size = textSize + padding;
         }
     }
 }
